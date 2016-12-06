fbba2872a23f8d54c1fc0add1944c7fb7e3e497b
Jose Ramon Navarro Marquez <ramonnavarropadilla@hotmail.com>
2016-12-06 14:35:01 -0600

    Se corrigio conexion con el servidor

--- IotDevice/index.js
+++ IotDevice/index.js
@@ -14,17 +14,9 @@
 
 //periodicActivity(); //call the periodicActivity function
 
-console.log('1');
+var socket = require('socket.io-client')('http://10.7.94.214:3000/');
+socket.on('connect', function(){console.log('Connected!');});
 
-// Connect to server
-var io = require('socket.io-client')
-var socket = io.connect('10.7.94.214:3000', {reconnect: true});
 
-console.log('2');
-
-// Add a connect listener
-socket.on('connect', function(socket) {
-    console.log('Connected!');
-});
-
-console.log('3');
+//socket.on('event', function(data){console.log('Connected!');});
+socket.on('disconnect', function(){console.log('Disconected!!');});
