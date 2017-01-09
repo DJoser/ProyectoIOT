fd7e42895c797a4c5833651616012416b0315d69
Jose Ramon Navarro Marquez <ramonnavarropadilla@hotmail.com>
2017-01-09 17:42:06 -0600

    Se agregaron las rutas personalizadas

--- IotWeb/js/init.js
+++ IotWeb/js/init.js
@@ -1,37 +1,74 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-angular.module('editorApp', [])
-    .controller('notasController', function($scope) {
-        $scope.notas = [
-            {titulo:'learn angular',contenido:"Contenido de la nota 1"},
-            {titulo:'build an angular app', contenido:"Contenido de la nota 2"}
-        ];
+var app = angular.module('iotApp', ["ngRoute"]);
 
-        $scope.addNote = function() {
-            $scope.notas.push({text:$scope.todoText, done:false});
-            $scope.todoText = '';
-        };
+app.controller('mainCtrl', function ($scope) {
+});
 
-        $scope.remaining = function() {
-            var count = 0;
-            angular.forEach($scope.notas, function(todo) {
-                count++;
-            });
-            return count;
-        };
+app.controller('tutorialCtrl', function ($scope) {
+});
 
-        $scope.archive = function() {
-            var oldTodos = $scope.notas;
-            $scope.notas = [];
-            angular.forEach(oldTodos, function(todo) {
-                //if (!todo.done) notasList.notas.push(todo);
-            });
-        };
-    });
+app.controller('dashboardCtrl', function ($scope) {
+});
+
+app.controller('dipositivosCtrl', function ($scope) {
+    $scope.notas = [
+        {titulo: 'learn angular', contenido: "Contenido de la nota 1"},
+        {titulo: 'build an angular app', contenido: "Contenido de la nota 2"}
+    ];
+
+    $scope.addNote = function () {
+        $scope.notas.push({text: $scope.todoText, done: false});
+        $scope.todoText = '';
+    };
+
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.notas, function (todo) {
+            count++;
+        });
+        return count;
+    };
+
+    $scope.archive = function () {
+        var oldTodos = $scope.notas;
+        $scope.notas = [];
+        angular.forEach(oldTodos, function (todo) {
+            //if (!todo.done) notasList.notas.push(todo);
+        });
+    };
+});
+app.config(function ($routeProvider,$locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "pages/tutorial.html",
+            controller: "tutorialCtrl"
+        })
+        .when("/dashboard", {
+            templateUrl: "pages/dashboard.html",
+            controller: "dashboardCtrl"
+        })
+        .when("/dispositivos", {
+            templateUrl: "pages/dispositivos.html",
+            controller: "dipositivosCtrl"
+        })
+        .when("/eventos", {
+            templateUrl: "pages/eventos.html",
+            controller: "eventosCtrl"
+        })
+        .when("/configuracion", {
+            templateUrl: "pages/configuracion.html",
+            controller: "eventosCtrl"
+        });
+
+    $routeProvider.otherwise( { redirectTo: '/' });
+
+    $locationProvider.hashPrefix('!');
+});
 
